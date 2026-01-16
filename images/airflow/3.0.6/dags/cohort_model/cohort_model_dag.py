be0f8e52ed41b671e94f64ed519311a1e8ede9bc
Taylor Rice <trice@tovala.com>
2026-01-16 12:41:03 -0600

    make create cohort model tables run for a list of models

--- images/airflow/3.0.6/dags/cohort_model/cohort_model_dag.py
+++ images/airflow/3.0.6/dags/cohort_model/cohort_model_dag.py
@@ -18,11 +18,8 @@ from common.macros.sql_templating import render_sql_from_file
     params={
         'channel_name': ''
     },
-    user_defined_macros={
-        "render_sql_from_file": render_sql_from_file,
-    }
 )
-def cohort_model():
+def cohortModel():
     '''Cohort Model
     Description: 
 
@@ -35,18 +32,40 @@ def cohort_model():
     '''
 
     MODELS = ['historical_meal_orders']
+
+    @task()
+    def generateParams(model_names: List[str]) -> List[Dict[str, Dict[str, str]]]:
+        '''
+        Set up parameters to pass to create cohort models sql template for each model in the supplied list.
+        Args:
+            model_names (str): list of models to be created
+        Output:
+            expanded_kwargs (str): list of dicts that are formatted kwargs to pass to SQLExecuteQueryOperator 
+        '''
+        expanded_kwargs = []
+
+        for model in model_names:
+            params = {'table_name': model, 'table_columns_file': f'queries/{model}.sql'}
+
+            expanded_kwargs.append({'params': params})
+
+        return expanded_kwargs
+
+    
     test_schema = SQLExecuteQueryOperator(
         task_id='create_test_schema',
         conn_id='snowflake',
         sql='CREATE SCHEMA IF NOT EXISTS TEST_TAYLOR_BRINE;'
     )
-    create_models = SQLExecuteQueryOperator(
+
+    generate_params = generateParams(MODELS)
+
+    create_models = SQLExecuteQueryOperator.partial(
         task_id='create_cohort_models', 
         conn_id='snowflake', 
         sql='templates/create_table.sql',
-        params={'table_name': 'historical_meal_orders', 'table_columns_file': 'queries/historical_meal_orders.sql'},
-    )
+    ).expand_kwargs(generate_params)
 
-    chain(test_schema, create_models)
+    chain([test_schema, generate_params], create_models)
 
-cohort_model()
+cohortModel()
