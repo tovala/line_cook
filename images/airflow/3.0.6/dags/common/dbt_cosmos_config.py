6da2917de662fcc57aeb0265108db4db8ba385fd
Taylor Rice <trice@tovala.com>
2026-01-26 12:20:49 -0600

    trying taskgroups

--- images/airflow/3.0.6/dags/common/dbt_cosmos_config.py
+++ images/airflow/3.0.6/dags/common/dbt_cosmos_config.py
@@ -4,36 +4,37 @@ from cosmos.profiles import SnowflakePrivateKeyPemProfileMapping
 
 AIRFLOW_HOME = os.environ["AIRFLOW_HOME"]
 
-dbt_project_config = ProjectConfig(
+DBT_PROJECT_CONFIG = ProjectConfig(
   dbt_project_path=f'{AIRFLOW_HOME}/spice_rack',
-  # models_relative_path='models', # test removing entirely 
   project_name='spice_rack',
   install_dbt_deps=True
 )
 
-# We want to have two profile configs: prod and test 
-# In prod, Airflows connections will get set up in MWAA 
-# We want to replicate the connection Jenkins currently has set up to run dbt pipelines 
+DBT_EXECUTION_CONFIG = ExecutionConfig(
+  execution_mode=ExecutionMode.VIRTUALENV,
+  dbt_executable_path=f'{AIRFLOW_HOME}/dbt_venv/bin/dbt'
+)
 
-dbt_profile_config = ProfileConfig(
+PROD_DBT_PROFILE_CONFIG = ProfileConfig(
+  profile_name='spice_rack',
+  target_name='prod',
+  profile_mapping = 
+    SnowflakePrivateKeyPemProfileMapping(
+        conn_id='snowflake',
+        profile_args={
+            'schema': 'prod',
+        },
+    ), 
+)
+
+TEST_DBT_PROFILE_CONFIG = ProfileConfig(
   profile_name='spice_rack',
   target_name='test',
   profile_mapping = 
     SnowflakePrivateKeyPemProfileMapping(
         conn_id='snowflake',
         profile_args={
-            # need a dynamic variable here for test branches 
             'schema': 'test_elly',
-            # in profiles.yml,,,,,
-            # test schema is 
-            # test_{{ env_var('BRANCH_SCHEMA_NAME') }}
-            # prod schema is 
-            # prod 
         },
     ), 
-)
-
-dbt_execution_config = ExecutionConfig(
-  execution_mode=ExecutionMode.VIRTUALENV,
-  dbt_executable_path=f'{AIRFLOW_HOME}/dbt_venv/bin/dbt'
 )
--- images/airflow/3.0.6/dags/ds_weekly_run/ds_weekly_run_dag.py
+++ images/airflow/3.0.6/dags/ds_weekly_run/ds_weekly_run_dag.py
@@ -1,37 +1,22 @@
 import datetime
-import os 
 
 from pendulum import duration
+from cosmos import DbtTaskGroup, RenderConfig, LoadMode 
+from airflow.sdk import dag
 
 from common.slack_notifications import bad_boy, good_boy
-from common.dbt_cosmos_config import dbt_project_config, dbt_profile_config, dbt_execution_config
-from cosmos import DbtDag, RenderConfig, LoadMode 
+from common.dbt_cosmos_config import DBT_PROJECT_CONFIG, DBT_EXECUTION_CONFIG, PROD_DBT_PROFILE_CONFIG
 
-
-ds_weekly_config = RenderConfig(
+DS_WEEKLY_RENDER_CONFIG = RenderConfig(
   selector='weekly_ds_run',
   load_method=LoadMode.DBT_LS,
   enable_mock_profile=False
 )
 
-dbt_run = DbtDag(
-  dag_id='weeklyDsRun',
-  project_config=dbt_project_config,
-  profile_config=dbt_profile_config,
-  execution_config=dbt_execution_config,
-  render_config=ds_weekly_config ,
-  operator_args={
-        "py_system_site_packages": False,
-        "py_requirements": ["dbt-snowflake"],
-        "install_deps": True,
-        "emit_datasets": False,
-        "execution_timeout": datetime.timedelta(minutes=10)
-  }, 
-  # Normal dag parameters 
+@dag(
   on_failure_callback=bad_boy,
   on_success_callback=good_boy,
   schedule = '0 21 * * 4', ## (non DST) 3 PM US/Central on Thursday
-  ## schedule = '0 20 * * 4' ## (DST) 3 PM US/Central on Thursday
   start_date=datetime.datetime(2026, 1, 15),
   catchup=False,
   default_args={
@@ -42,4 +27,21 @@ dbt_run = DbtDag(
   },
     tags=['data_science']
 )
+def weeklyDsRun():
+  dbt_run = DbtTaskGroup(
+    group_id='weekly_ds_run',
+    project_config=DBT_PROJECT_CONFIG,
+    profile_config=PROD_DBT_PROFILE_CONFIG,
+    execution_config=DBT_EXECUTION_CONFIG,
+    render_config=DS_WEEKLY_RENDER_CONFIG ,
+    operator_args={
+          "py_system_site_packages": False,
+          "py_requirements": ["dbt-snowflake"],
+          "install_deps": True,
+          "emit_datasets": False,
+          "execution_timeout": datetime.timedelta(minutes=10)
+    },
+  )
+
+weeklyDsRun()
 
