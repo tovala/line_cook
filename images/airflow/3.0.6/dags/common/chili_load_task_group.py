7d1bd6976efdc52c1d361953f8414733b41bef29
Taylor Rice <trice@tovala.com>
2026-02-05 17:22:22 -0600

    common chili load task group

---
+++ images/airflow/3.0.6/dags/common/chili_load_task_group.py
@@ -0,0 +1,30 @@
+from typing import Dict, List
+
+from airflow.sdk import task_group, chain
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.snowflake.transfers.copy_into_snowflake import CopyFromExternalStageToSnowflakeOperator
+
+@task_group()
+def loadIntoChili(file_type: str, stage_name: str, s3_url: str, sf_storage_integration: str, copy_table_params: List[Dict[str, str]]):
+  create_external_stage = SQLExecuteQueryOperator(
+      task_id=f'create_{stage_name}', 
+      conn_id='snowflake', 
+      sql='create_stage.sql',
+      params={
+        'parent_database': 'MASALA',
+        'schema_name': 'CHILI_V2',
+        'stage_name': stage_name,
+        'url': s3_url,
+        'storage_integration': sf_storage_integration,
+        'file_type': file_type,
+      },
+    )
+
+  copy_tables = CopyFromExternalStageToSnowflakeOperator.partial(
+    task_id='copyTable', 
+    snowflake_conn_id='snowflake',
+    stage=f'MASALA.CHILI_V2.{stage_name}',
+    file_format=f"(TYPE = '{file_type}')",
+  ).expand_kwargs(copy_table_params)
+
+  chain(create_external_stage, copy_tables)
