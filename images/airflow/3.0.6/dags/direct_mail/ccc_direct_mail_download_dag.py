8aac816f51527a66f9e3be753a7d6a94ccdc9c52
jvmakin <jane@tovala.com>
2026-02-19 14:07:50 -0600

    well it sort of works

--- images/airflow/3.0.6/dags/direct_mail/ccc_direct_mail_download_dag.py
+++ images/airflow/3.0.6/dags/direct_mail/ccc_direct_mail_download_dag.py
@@ -3,6 +3,7 @@ from airflow.sdk import dag, task, chain
 from airflow.timetables.trigger import CronTriggerTimetable
 from common.slack_notifications import bad_boy, good_boy, slack_param
 from airflow.providers.amazon.aws.transfers.sftp_to_s3 import SFTPToS3Operator
+from airflow.providers.sftp.hooks.sftp import SFTPHook
 
 @dag(
   dag_id='ccc_direct_mail_download',
@@ -17,8 +18,8 @@ from airflow.providers.amazon.aws.transfers.sftp_to_s3 import SFTPToS3Operator
 )
 def CCCDirectMailDownload():
   '''CCC Direct Mail Import
-  Description: Grabs file from CCC SFTP and sends to S3
-  # 1. Generates the start date, end date, and filename
+  Description: Grabs files from CCC SFTP and sends to S3
+  # 1. Grabs files and uploads to S3
 
   Schedule: Daily at 3:05 AM 
 
@@ -28,12 +29,28 @@ def CCCDirectMailDownload():
 
   '''
 
-  sftp_to_s3 = SFTPToS3Operator(
+  @task()
+  def fetchSFTPFiles(filename_pattern: str = 'CLIENT_OUTPUT_') -> list:
+    sftp_hook = SFTPHook(ssh_conn_id='direct_mail_ccc_sftp')
+    files = sftp_hook.list_directory('.')
+
+    return [f for f in files if filename_pattern in f]
+
+  @task()
+  def generateExpandDictionary(file_list: list) -> list:
+    sftp_dict = [{'sftp_path': './'+ f, 's3_key': 'data_downloads/' + f} for f in file_list]
+    return sftp_dict
+  
+  sftp_filenames = fetchSFTPFiles()
+  sftp_dict = generateExpandDictionary(sftp_filenames)
+
+
+  sftp_to_s3 = SFTPToS3Operator.partial(
     task_id='sftp_to_s3',
     sftp_conn_id='direct_mail_ccc_sftp',
-    sftp_path='./CLIENT_OUTPUT*',
     s3_bucket='tovala-ccc-direct-mail',
-    s3_key='data_download/'
-  )
+  ).expand_kwargs(sftp_dict)
 
-  sftp_to_s3
+  #TODO: Either convert to CSV or load as pipe delimited 
+
+CCCDirectMailDownload()
