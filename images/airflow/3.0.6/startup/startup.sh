1518ce0844ad02b23b52d007f03c975f18133de6
jvmakin <jane@tovala.com>
2026-02-24 09:20:50 -0600

    removing line from startup.sh

--- images/airflow/3.0.6/startup/startup.sh
+++ images/airflow/3.0.6/startup/startup.sh
@@ -5,5 +5,3 @@ export DBT_VENV_PATH="${AIRFLOW_HOME}/dbt_venv"
 python3 -m venv "${DBT_VENV_PATH}"
 
 ${DBT_VENV_PATH}/bin/pip install dbt-snowflake
-
-chmod +x ${AIRFLOW_HOME}/dags/direct_mail/scripts/txt_to_gz.py
