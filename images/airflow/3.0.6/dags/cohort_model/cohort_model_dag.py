43982f4ca444f39baa484ca0e2dfa85692b6eefe
Taylor Rice <trice@tovala.com>
2026-02-25 10:12:34 -0600

    wip

--- images/airflow/3.0.6/dags/cohort_model/cohort_model_dag.py
+++ images/airflow/3.0.6/dags/cohort_model/cohort_model_dag.py
@@ -2,7 +2,6 @@ import os
 
 from airflow.sdk import dag, chain, task_group, Param
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.providers.snowflake.transfers.copy_into_snowflake import CopyFromExternalStageToSnowflakeOperator
 
 from common.slack_notifications import slack_param
 from cohort_model.snapshot_to_s3_task_group import snapshotSnowflakeToS3
@@ -25,90 +24,101 @@ AIRFLOW_HOME = os.environ["AIRFLOW_HOME"]
     template_searchpath = f'{AIRFLOW_HOME}/dags/common/templates'
 )
 def cohortModel():
-    '''Cohort Model
-    Description: 
+  '''Cohort Model
+  Description: 
 
-    Schedule: TBD
+  Schedule: TBD
 
-    Dependencies:
+  Dependencies:
 
-    Variables:
+  Variables:
 
-    '''
+  '''
 
-    MODELS = ['actual_oven_sales', 'combined_oven_sales', 'historical_meal_orders', 'projected_oven_sales', 'weekly_meal_and_meal_order_counts']
+  INPUT_MODELS = ['actual_oven_sales', 'combined_oven_sales', 'historical_meal_orders', 'projected_oven_sales', 'weekly_meal_and_meal_order_counts']
+  CSV_INPUTS = ['attach_rates', 'cohort_mix_projections', 'constants', 'daily_oven_d2c_sales_splits', 'daily_oven_sales_projections', 'holiday_term_skip_adjustments', 'meal_retention_curves', 'meals_per_order_assumptions', 'model_order_retention_curves']
     
-    #### Custom Task Definitions
-    @task_group(group_id='create_temp_table')
-    def createTempTable(model: str) -> None:
-        create_empty_model = SQLExecuteQueryOperator(
-            task_id='create_cohort_models', 
-            conn_id='snowflake', 
-            sql='create_table.sql',
-            params={
-                'table': model,
-                'table_columns_file': f'queries/{model}/table_columns.sql'
-            }
-        )
+#### Custom Task Definitions
+@task_group(group_id='create_temp_table')
+def createTempTable(model: str) -> None:
+  create_empty_model = SQLExecuteQueryOperator(
+    task_id='create_cohort_models', 
+    conn_id='snowflake', 
+    sql='create_table.sql',
+    params={
+      'table': model,
+      'table_columns_file': f'queries/{model}/table_columns.sql'
+    }
+  )
 
-        load_model_default = SQLExecuteQueryOperator(
-            task_id='load_model_default', 
-            conn_id='snowflake', 
-            sql='create_table.sql',
-            params={
-                'table': model,
-                'table_columns_file': f'queries/{model}/default_input.sql'
-            }
-        )
+  load_model_default = SQLExecuteQueryOperator(
+    task_id='load_model_default', 
+    conn_id='snowflake', 
+    sql='create_table.sql',
+    params={
+      'table': model,
+      'table_columns_file': f'queries/{model}/default_input.sql'
+    }
+  )
         
-        # TODO: Add S3 override here - maybe add an override param? like a list of models w override - set comparison to standard list to avoid doing both?
-        # copy_from_csv = CopyFromExternalStageToSnowflakeOperator.partial(
-        #   task_id='copyTable', 
-        #   snowflake_conn_id='snowflake',
-        #   stage='MASALA.CHILI_V2.cohort_model_input_stage',
-        #   file_format="(TYPE = 'csv')",
-        #   params={
-        #       'table': f'CHILI_V2.{model}',
-        #       'pattern': f'.*{model}.*.parquet'
-        #   }
-        # )
+  # TODO: Add S3 override here - maybe add an override param? like a list of models w override - set comparison to standard list to avoid doing both?
+  # copy_from_csv = CopyFromExternalStageToSnowflakeOperator.partial(
+  #   task_id='copyTable', 
+  #   snowflake_conn_id='snowflake',
+  #   stage='MASALA.CHILI_V2.cohort_model_input_stage',
+  #   file_format="(TYPE = 'csv')",
+  #   params={
+  #       'table': f'CHILI_V2.{model}',
+  #       'pattern': f'.*{model}.*.parquet'
+  #   }
+  # )
 
-        chain(create_empty_model, load_model_default)
+  chain(create_empty_model, load_model_default)
 
-    #### Task Instances
-    cohort_model_input_stage = SQLExecuteQueryOperator(
-      task_id='createCohortModelInputStage', 
-      conn_id='snowflake', 
-      sql='create_stage.sql',
-      params={
-        'parent_database': 'MASALA',
-        'schema_name': 'CHILI_V2',
-        'stage_name': 'cohort_model_input_stage',
-        'url': 's3://tovala-data-cohort-model/input/',
-        'storage_integration': 'COHORT_MODEL_STORAGE_INTEGRATION',
-        'file_type': 'csv',
-      },
-    )
+  #### Task Instances
+  cohort_model_input_stage = SQLExecuteQueryOperator(
+    task_id='createCohortModelInputStage', 
+    conn_id='snowflake', 
+    sql='create_stage.sql',
+    params={
+      'parent_database': 'MASALA',
+      'schema_name': 'CHILI_V2',
+      'stage_name': 'cohort_model_input_stage',
+      'url': 's3://tovala-data-cohort-model/input/',
+      'storage_integration': 'COHORT_MODEL_STORAGE_INTEGRATION',
+      'file_type': 'csv',
+    },
+  )
+  
+  create_temp_schema = SQLExecuteQueryOperator(
+    task_id='create_temp_schema',
+    conn_id='snowflake',
+    sql='CREATE SCHEMA IF NOT EXISTS {{ params.schema }};'
+  )
+
+  create_temp_tables = createTempTable.expand(INPUT_MODELS)
+
+  # TODO: pull input csvs from S3 to tables in temp schema
+  copy_from_csv = CopyFromExternalStageToSnowflakeOperator.partial(
+  task_id='copyTable', 
+    snowflake_conn_id='snowflake',
+    stage='MASALA.CHILI_V2.cohort_model_input_stage',
+    file_format="(TYPE = 'csv')",
+    params={
+      'table': f'CHILI_V2.{model}',
+      'pattern': f'.*{model}.*.parquet'
+    }
+  )
+
+  snapshot_inputs = snapshotSnowflakeToS3()
+
+  drop_temp_schema = SQLExecuteQueryOperator(
+    task_id='drop_temp_schema',
+    conn_id='snowflake',
+    sql='DROP SCHEMA {{ params.schema }};'
+  )
     
-    create_temp_schema = SQLExecuteQueryOperator(
-        task_id='create_temp_schema',
-        conn_id='snowflake',
-        sql='CREATE SCHEMA IF NOT EXISTS {{ params.schema }};'
-    )
 
-    create_temp_tables = createTempTable.expand(MODELS)
-
-    # TODO: pull input csvs from S3 to tables in temp schema
-
-    snapshot_inputs = snapshotSnowflakeToS3()
-
-    drop_temp_schema = SQLExecuteQueryOperator(
-        task_id='drop_temp_schema',
-        conn_id='snowflake',
-        sql='DROP SCHEMA {{ params.schema }};'
-    )
-    
-
-    chain([create_temp_schema, cohort_model_input_stage], create_temp_tables, snapshot_inputs, drop_temp_schema)
+  chain([create_temp_schema, cohort_model_input_stage], create_temp_tables, snapshot_inputs, drop_temp_schema)
 
 cohortModel()
