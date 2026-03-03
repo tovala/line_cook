cfac50dd825691a2668acf3f66579abf13f998dd
Taylor Rice <trice@tovala.com>
2026-03-03 10:55:27 -0600

    add custom Params

--- images/airflow/3.0.6/dags/cohort_model/cohort_model_params.py
+++ images/airflow/3.0.6/dags/cohort_model/cohort_model_params.py
@@ -1,4 +1,46 @@
+from typing import List
+
 from airflow.sdk import Param
 
-def mealsPerOrderAssumptionsParam(order_type: str, default_val: float):
-  return Param(default_val, title=f'Default for {order_type}', section='Meals Per Order Assumptions', type='number')
+class mealsPerOrderAssumptionsParam(Param):
+  '''
+  Class to set meals per order assumption Params in Airflow. Extends Airflow Param.
+
+  :param order_type: Name of the order type for the current assumption value
+  :param default: Type float. Default value for the given order type param. (0.000 if not specified)
+  '''
+  def __init__(
+    self,
+    order_type: str,
+    default: float = 0.000,
+    **kwargs
+  ):
+    super().__init__(
+      default,
+      title=f'Default for {order_type}',
+      section='Meals Per Order Assumptions',
+      type='number',
+      **kwargs
+    )
+
+class sixWeekAttachRateParam(Param):
+  '''
+  Class to set six week attach rate Params in Airflow. Extends Airflow Param.
+
+  :param order_type: Name of the order type for the current assumption value
+  :param default: List of default values for the given order type param.
+  '''
+  def __init__(
+    self,
+    order_type: str,
+    default: List[float] = [0.000, 0.000, 0.200, 0.050, 0.000, 0.000],
+    **kwargs
+  ):
+    super().__init__(
+      default,
+      title=f'Default for {order_type}',
+      description='Attach rates for first six weeks.',
+      section='Six Week Attach Rates',
+      type='array',
+      **kwargs
+    )
