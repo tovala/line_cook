2f2bdf7f7205c74d6002a760b492b6b32fa2d44d
Taylor Rice <trice@tovala.com>
2026-04-17 15:03:38 -0500

    fix

--- images/airflow/3.0.6/dags/cohort_model/queries/dataframes/future_term_cohort_age_matrix.sql
+++ images/airflow/3.0.6/dags/cohort_model/queries/dataframes/future_term_cohort_age_matrix.sql
@@ -17,12 +17,12 @@ SELECT
     , COALESCE(last_known_term_age, 0) AS last_known_term_age
 FROM "{{ params.runtime_schema_prefix }}_{{ run_id }}".FUTURE_COHORT_INITIAL_ORDER_PREDICTIONS AS ft 
 FULL JOIN last_known_age AS lka ON ft.cohort = lka.cohort),
-projection_terms_array AS (
+projection_terms AS (
     SELECT
-        ARRAY_AGG(term_id) 
+        ARRAY_AGG(term_id) AS arr
     FROM mugwort.future_terms 
     WHERE term_id <= (SELECT MAX(term_id) FROM mugwort.combined_oven_sales)
 )
 SELECT cohort,
-TRANSFORM(projection_terms_array, ter INT -> (ter - last_known_term) + last_known_term_age) AS future_term_cohort_age
+TRANSFORM(projection_terms.arr, ter INT -> (ter - last_known_term) + last_known_term_age) AS future_term_cohort_age
 FROM all_model_cohorts WHERE cohort IS NOT NULL ORDER BY cohort;
