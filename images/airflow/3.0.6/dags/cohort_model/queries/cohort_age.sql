e203f242030991e9cbb729afec2181be16e98f0d
Taylor Rice <trice@tovala.com>
2026-04-17 13:46:01 -0500

    Update images/airflow/3.0.6/dags/cohort_model/queries/cohort_age.sql
    Co-authored-by: Jane <jane@tovala.com>
    Signed-off-by: Taylor Rice <taylor.rice2014@gmail.com>

--- images/airflow/3.0.6/dags/cohort_model/queries/cohort_age.sql
+++ images/airflow/3.0.6/dags/cohort_model/queries/cohort_age.sql
@@ -3,7 +3,7 @@ AS
 SELECT DISTINCT 
   cohort 
   , term_id 
-  , (cohort_week_without_holidays - 1) as cohort_age
+  , (cohort_week_without_holidays - 1) AS cohort_age
 FROM season.customer_term_summary
 WHERE cohort_week_without_holidays IS NOT NULL
 ORDER BY 1,2;
