4852a0d2864c9da7302bae47a1c4576ae5276336
Taylor Rice <trice@tovala.com>
2026-04-23 11:22:50 -0500

    legacy comp review

--- images/airflow/3.0.6/dags/cohort_model/yarrow/cohort_model_default_inputs_dag.py
+++ images/airflow/3.0.6/dags/cohort_model/yarrow/cohort_model_default_inputs_dag.py
@@ -6,6 +6,7 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from common.slack_notifications import slack_param
 from cohort_model.yarrow.extended_monthly_oven_sales_predictions_task_group import extendedMonthlyOvenSalesPredictions
 from cohort_model.yarrow.cohort_mix_projections_task_group import cohortMixProjections
+from cohort_model.yarrow.retention_curves_task_group import retentionCurves
 
 
 AIRFLOW_HOME = os.environ["AIRFLOW_HOME"]
