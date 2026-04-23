5ac1d1237e00a3daebaa5bcabd41806a11a04141
Taylor Rice <trice@tovala.com>
2026-04-23 11:37:57 -0500

    fix batch indexing off by one error

--- images/airflow/3.0.6/dags/experian/queries/customer_batch.sql
+++ images/airflow/3.0.6/dags/experian/queries/customer_batch.sql
@@ -1,5 +1,6 @@
 SET num_batch_size = {{ params.batch_size }};
+SET current_batch = {{ ti.map_index }} + 1;
 SELECT 
   '{' || LISTAGG('"rec' || COALESCE(NULLIFZERO(row_number%$num_batch_size),$num_batch_size) || '":"' || request_body || '"', ',') WITHIN GROUP (ORDER BY row_number) || '}'
 FROM brine.experian_customers_temp
-WHERE row_number BETWEEN ({{ params.batch_size }} * {{ ti.map_index }}) - {{ params.batch_size }} + 1 AND {{ params.batch_size }} * {{ ti.map_index }};
+WHERE row_number BETWEEN ({{ params.batch_size }} * $current_batch) - {{ params.batch_size }} + 1 AND {{ params.batch_size }} * $current_batch;
