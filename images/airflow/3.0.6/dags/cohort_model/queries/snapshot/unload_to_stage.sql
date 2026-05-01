daf3071c0347fe573bdddc5377ed38a874ffe38a
Taylor Rice <trice@tovala.com>
2026-05-01 11:08:17 -0500

    update snapshot destination

--- images/airflow/3.0.6/dags/cohort_model/queries/snapshot/unload_to_stage.sql
+++ images/airflow/3.0.6/dags/cohort_model/queries/snapshot/unload_to_stage.sql
@@ -1,3 +1,3 @@
-COPY INTO @{{ params.database}}.{{ params.schema }}.{{ params.stage }}/outputs/{{ run_id }}/
+COPY INTO @{{ params.database}}.{{ params.schema }}.{{ params.stage }}/{{ run_id }}/
 FROM {{ params.database }}."{{ params.runtime_schema_prefix }}_{{ run_id }}".{{ params.table }}
-FILE_FORMAT = (TYPE = PARQUET);
+FILE_FORMAT = (TYPE = {{ params.file_format }});
