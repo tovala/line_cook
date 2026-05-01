2e820e885bd43b6fe1420fa7a69671b797f4ad9c
Taylor Rice <trice@tovala.com>
2026-05-01 12:36:20 -0500

    templatize file extension on snapshots

--- images/airflow/3.0.6/dags/cohort_model/queries/snapshot/unload_to_stage.sql
+++ images/airflow/3.0.6/dags/cohort_model/queries/snapshot/unload_to_stage.sql
@@ -1,4 +1,4 @@
-COPY INTO @{{ params.database}}.{{ params.schema }}.{{ params.stage }}/{{ run_id }}/runtime_{{ params.table }}.snappy.parquet
+COPY INTO @{{ params.database}}.{{ params.schema }}.{{ params.stage }}/{{ run_id }}/runtime_{{ params.table }}.{{ params.file_format }}
 FROM {{ params.database }}."{{ params.runtime_schema_prefix }}_{{ run_id }}".{{ params.table }}
 FILE_FORMAT = (TYPE = {{ params.file_format }})
 SINGLE = TRUE
